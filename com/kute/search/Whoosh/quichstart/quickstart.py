e2d2c2e3668a2da2ecd2449da762136a4f3012dc
kute <kutekute00@gmail.com>
2016-08-08 22:11:14 +0800

    add some note

--- com/kute/search/Whoosh/quichstart/quickstart.py
+++ com/kute/search/Whoosh/quichstart/quickstart.py
@@ -6,12 +6,16 @@
 
 """
 
+入门例子
+
 """
 
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 from kute.caltime.caltime import perf_counter, process_time
+import random
+import logging
 import os
 import sys
 
@@ -23,18 +27,34 @@ def create_index_dir(dirname):
     return path
 
 
+class MySchema(SchemaClass):
+    """
+        schema创建的第二种方式
+    """
+    title = TEXT(stored=True)
+    path = ID(stored=True)
+    content = TEXT(stored=True)
+
+
 @process_time
 def create_schema():
-    return Schema(
-        title=TEXT(stored=True),
-        path=ID(stored=True),
-        content=TEXT(stored=True)
-    )
+    """随机使用两种创建schema的方式"""
+    if random.randint(0, 1):
+        return Schema(
+            title=TEXT(stored=True),
+            path=ID(stored=True),
+            content=TEXT(stored=True)
+        )
+    return MySchema
 
 
 @process_time
 def create_index(schema, indexdir):
-    return create_in(dirname=indexdir, schema=schema)
+    ix = open_dir(indexdir)
+    if not ix:
+        logging.info("open failed, then created")
+        ix = create_in(dirname=indexdir, schema=schema)
+    return ix
 
 
 @process_time
@@ -49,22 +69,35 @@ def create_writer(index, maxnumber):
             path="./path-%s" % i,
             content="content%s" % i
         )
-    writer.add_document(title="index title", _stored_title="store title", content="content8")
+    # writer.add_document(title="index title", _stored_title="store title", content="content8")
     writer.commit()
 
 
 @process_time
-def search(ix):
+def search(ix, qstr):
+    """
+
+    parser.parse(u"render shade animate")  <=> And([Term("content", "render"), Term("content", "shade"), Term("content", "animate")])
+    parser.parse(u"render OR (title:shade keyword:animate)")  <=>  Or([Term("content", "render"), And([Term("title", "shade"), Term("keyword", "animate")])])
+    parser.parse(u"rend*")  <=>  Prefix("content", "rend")
+
+    """
     with ix.searcher() as searcher:
-        query = QueryParser("content", ix.schema).parse("content8")
+        query = QueryParser("content", ix.schema).parse(qstr)
         results = searcher.search(query)
         print([r for r in results])
 
 
 if __name__ == '__main__':
+    # 1. create schema
     schema = create_schema()
+
+    # 2. create index by schema
     index = create_index(schema, create_index_dir("indexdir"))
 
+    # 3. add document
     create_writer(index, 10000)
 
-    search(index)
+    # 4. search
+    querystr = "content8"
+    search(index, querystr)
