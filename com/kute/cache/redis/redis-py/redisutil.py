4ba4c6b4abd0965877a272afe0ec39167c43c068
kute <kutekute00@gmail.com>
2016-08-23 22:03:10 +0800

    redis util

--- com/kute/cache/redis/redis-py/redisutil.py
+++ com/kute/cache/redis/redis-py/redisutil.py
@@ -11,6 +11,7 @@
 
 import redis
 from kute.easylog.easylog import geteasylog
+import time
 
 
 easylog = geteasylog()
@@ -47,14 +48,79 @@ class RedisUtil(object):
     def keys(self, pattern="*"):
         return self._connection_redis().keys(pattern=pattern)
 
-    def set(self, name, value):
-        return self._connection_redis().set(name=name, value=value)
+    def exists(self, key):
+        return self._connection_redis().exists(name=key)
 
-    def get(self, name):
-        return self._connection_redis().get(name=name)
+    def existsbatch(self, *keys):
+        return [self.exists(key) for key in keys]
+
+    def existsbatchbypipeline(self, *keys):
+        with self.pipeline() as pipe:
+            for key in keys:
+                pipe.exists(key)
+            return pipe.execute()
+
+    def delete(self, *keys):
+        return self._connection_redis().delete(*keys)
+
+    def set(self, key, value):
+        return self._connection_redis().set(name=key, value=value)
+
+    def get(self, key):
+        return self._connection_redis().get(name=key)
 
     def pipeline(self, transaction=True):
         return self._connection_redis().pipeline(transaction=transaction)
+
+    def incr(self, key, value=1):
+        self._connection_redis().incr(name=key, amount=value)
+
+    def incrby(self, key, value):
+        self._connection_redis().incrby(key, value)
+
+    def zadd(self, key, score, member):
+        self._connection_redis().zadd(key, score, member)
+
+    def zaddbatch(self, key, paramdict):
+        """
+        批量添加
+        : paramdict 字典类型, 如{"key": score}
+        """
+        if not key or not paramdict or not isinstance(paramdict, dict):
+            return
+        with self.pipeline() as pipe:
+            for member, score in paramdict.items():
+                pipe.zadd(key, score, member)
+            pipe.execute(raise_on_error=True)
+
+    def zrange(self, name, start, end, desc=False, withscores=False,
+               score_cast_func=float):
+        """
+        Return a range of values from sorted set ``name`` between
+        ``start`` and ``end`` sorted in ascending order.
+
+        ``start`` and ``end`` can be negative, indicating the end of the range.
+
+        ``desc`` a boolean indicating whether to sort the results descendingly
+
+        ``withscores`` indicates to return the scores along with the values.
+        The return type is a list of (value, score) pairs
+
+        ``score_cast_func`` a callable used to cast the score return value
+        """
+        self._connection_redis().zrange(name, start, end, desc, withscores, score_cast_func)
+
+    def zrank(self, key, member):
+        return self._connection_redis().zrank(key, member)
+
+    def zcount(self, key, min, max):
+        return self._connection_redis().zcount(key, min, max)
+
+    def zcard(self, key):
+        return self._connection_redis().zcard(key)
+
+    def execute_command(self, *args, **options):
+        self._connection_redis().execute_command(*args, **options)
 
     def regist_lua_script(self, lua=None):
         """
@@ -65,8 +131,13 @@ class RedisUtil(object):
 
 
 def main():
-    ru = RedisUtil(host="localhost", password="kuteredis")
-    easylog.info(ru.keys())
+    ru = RedisUtil(password="kuteredis")
+    myzsetkey = "kute_zset"
+    # ru.zadd(myzsetkey, time.clock(), "chinamember")
+    mydict = {"china2": float(0.1), "japan2": float(0.2)}
+    # ru.zaddbatch(myzsetkey, mydict)
+    print(ru.existsbatch("a", "b", "kute_zset"))
+    print(ru.existsbatchbypipeline("a", "b", "kute_zset"))
 
 
 if __name__ == "__main__":
