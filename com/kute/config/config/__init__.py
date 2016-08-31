a315cab6a2161841b647b458f2f791b242f06aa8
kute <kutekute00@gmail.com>
2016-08-31 22:19:36 +0800

    config module

---
+++ com/kute/config/config/__init__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# __author__ = 'kute'
+# __mtime__ = '16/8/31 22:09'
+
+"""
+
+https://www.red-dove.com/config-doc/
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
