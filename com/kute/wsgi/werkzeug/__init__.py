9526fbdc169fbcfbb27ed5c1f890c1b168395d14
kute <kutekute00@gmail.com>
2016-10-08 22:24:53 +0800

    werkzeug: util package for wsgi

---
+++ com/kute/wsgi/werkzeug/__init__.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# __author__ = 'kute'
+# __mtime__ = '16/10/8 21:04'
+
+"""
+
+WSGI  工具集
+
+https://github.com/pallets/werkzeug
+
+http://werkzeug.pocoo.org/
+
+http://werkzeug.pocoo.org/docs/0.11/
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
