988742d3b254c8d5278a77b8629312f71d43ebcb
longbai <longbai@corp.netease.com>
2016-12-13 14:45:06 +0800

    logging

--- com/kute/log/logging/test.py
+++ com/kute/log/logging/test.py
@@ -20,10 +20,18 @@ logging.basicConfig(level=logging.DEBUG,
 def main():
     mylog = logging.getLogger(__name__)
     mylog.setLevel(logging.DEBUG)
+
+    # 控制台输出
     consoleHandler = logging.StreamHandler()
     myformat = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
     consoleHandler.setFormatter(myformat)
+
+    # 输出到文件
+    filehandler = logging.FileHandler("data.log", encoding="utf-8")
+    filehandler.setFormatter(myformat)
+
     mylog.addHandler(consoleHandler)
+    mylog.addHandler(filehandler)
     mylog.info("tset")
 
 if __name__ == '__main__':
