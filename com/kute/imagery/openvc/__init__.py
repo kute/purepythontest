839b3ff065360f58f76853530c1ee5b6be7ded60
longbai <longbai@corp.netease.com>
2017-02-27 18:23:50 +0800

    add openvc

---
+++ com/kute/imagery/openvc/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0
+@ __author__: kute 
+@ __file__: __init__.py.py
+@ __mtime__: 2017/2/27 18:20
+
+http://opencv.org
+https://github.com/spmallick/learnopencv
+
+"""
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
