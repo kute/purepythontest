c8685c34c4f84be6ab150b10f004e7f5b7d2fc85
longbai <longbai@corp.netease.com>
2017-03-07 19:21:54 +0800

    add wordcloud

---
+++ com/kute/data/wordcloud/__init__.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0
+@ __author__: kute 
+@ __file__: __init__.py.py
+@ __mtime__: 2017/3/7 18:10
+
+词云 文字云
+
+https://github.com/amueller/word_cloud
+
+http://mp.weixin.qq.com/s/HysMAAPMY2zLilQVnTUE5A
+
+"""
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
