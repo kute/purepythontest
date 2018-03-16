7ce2a88300086e4dbbd4c90498a08c4db0007fe6
bailong03 <bailong03@meituan.com>
2018-03-16 19:14:47 +0800

    add requirements

---
+++ com/kute/util/merge_maven_repo.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# __author__ = 'kute'
+# __mtime__ = '2018/3/16 下午5:19'
+
+"""
+
+"""
+
+import shutil
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
